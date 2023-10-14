27414efcea1b1df1deb7873a23b15d648e784222
Steven Xie <xies1999@hotmail.com>
2023-10-14 14:39:20 -0400

    initialized jwt

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 const db = require('../models')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
 const { User } = db
 
@@ -13,7 +14,21 @@ router.post('/', async(req, res) => {
             message: `Could not find a user with the provided username and password`
         })
     } else {
-        res.json({ user })
+        const result = await jwt.encode(process.env.JWT_SECRET, { id: user.userId })
+        res.json({ user: user, token: result.value })
+    }
+})
+
+router.get('/profile', async (req, res) => {
+    try {
+        let user = await User.findOne({
+            where: {
+                userId:__  
+            }
+        })
+        res.json(user)
+    } catch {
+        res.json(null)
     }
 })
 
--- frontend/src/contexts/CurrentUser.js
+++ frontend/src/contexts/CurrentUser.js
@@ -6,7 +6,14 @@ export const CurrentUser = createContext()
 function CurrentUserProvider({ children }){
 
     const [currentUser, setCurrentUser] = useState(null)
-
+    useEffect(()=>{
+        const getLoggedInUser = async()=>{
+        let response = await fetch('http://localhost:5000/authentication/profile')
+        let user = await response.json()
+        setCurrentUser(user)
+        }
+        getLoggedInUser()
+    },[])
     return (
         <CurrentUser.Provider value={{ currentUser, setCurrentUser }}>
             {children}
