b17d15e22d48a1bf594cda6b496c5e0913614241
Steven Xie <xies1999@hotmail.com>
2023-10-17 22:01:19 -0400

    implemented jwt

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -1,7 +1,7 @@
 const router = require('express').Router()
 const db = require('../models')
 const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken')
+const jwt = require('json-web-token')
 
 const { User } = db
 
@@ -21,12 +21,27 @@ router.post('/', async(req, res) => {
 
 router.get('/profile', async (req, res) => {
     try {
-        let user = await User.findOne({
-            where: {
-                userId:__  
-            }
-        })
-        res.json(user)
+        // Split the authorization header into [ "Bearer", "TOKEN" ]:
+        const [authenticationMethod, token] = req.headers.authorization.split(' ')
+
+        // Only handle "Bearer" authorization for now 
+        //  (we could add other authorization strategies later):
+        if (authenticationMethod == 'Bearer') {
+
+            // Decode the JWT
+            const result = await jwt.decode(process.env.JWT_SECRET, token)
+
+            // Get the logged in user's id from the payload
+            const { id } = result.value
+
+            // Find the user object using their id:
+            let user = await User.findOne({
+                where: {
+                    userId: id
+                }
+            })
+            res.json(user)
+        }
     } catch {
         res.json(null)
     }
